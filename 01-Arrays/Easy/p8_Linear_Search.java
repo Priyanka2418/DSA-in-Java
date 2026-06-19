f47e3701fe25a73f60fa2b3f6c09164be285693e
Priyanka Gupta <priyanka142024@gmail.com>
2026-06-19 17:28:42 +0530

    Implement Linear Search algorithm

---
+++ 01-Arrays/Easy/p8_Linear_Search.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class p8_Linear_Search {
+    public static int linearSearch(int arr[], int num){
+        for(int i =0;i<arr.length;i++){
+            if(arr[i]==num) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        int num = 30;
+        System.out.println(Arrays.toString(arr));
+
+        int result = linearSearch(arr, num);
+
+        System.out.println(result);
+    }
+}
