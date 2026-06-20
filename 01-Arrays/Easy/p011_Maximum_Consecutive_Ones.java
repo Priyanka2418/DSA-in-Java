c2c7a59d75fc8a8c6e76b6aa57490f093b962452
Priyanka Gupta <priyanka142024@gmail.com>
2026-06-20 07:54:41 +0530

    Add Maximum Consecutive Ones solution

---
+++ 01-Arrays/Easy/p011_Maximum_Consecutive_Ones.java
@@ -0,0 +1,30 @@
+/*
+  PROBLEM: Maximum Consecutive Ones
+  Approach:
+  Traverse the array and maintain a count of consecutive 1s.
+  Reset the count whenever a 0 is encountered and keep track
+
+  Time Complexity: O(n)
+  Space Complexity: O(1)
+*/
+
+public class p011_Maximum_Consecutive_Ones {
+    public static int findMaxConsecutiveOnes(int arr[]){
+        int maxCount=0;
+        int count=0;
+        for(int i =0;i<arr.length;i++){
+            if(arr[i]==1){
+                count++;
+            }else{
+                count=0;
+            }
+            maxCount=Math.max(maxCount,count);
+        }
+        return maxCount;
+    }
+    public static void main(String[] args) {
+        int nums[]={1,0,1,1,1,0,0,0,1,1};
+
+        System.out.println("The maximum consecutive 1's are "+ findMaxConsecutiveOnes(nums));
+    }
+}
