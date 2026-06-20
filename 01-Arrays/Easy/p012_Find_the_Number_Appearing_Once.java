addebc496d8809437b4d4dda26c6d6389cab3ac4
Priyanka Gupta <priyanka142024@gmail.com>
2026-06-20 07:54:58 +0530

    Add Number Appearing Once solution

---
+++ 01-Arrays/Easy/p012_Find_the_Number_Appearing_Once.java
@@ -0,0 +1,26 @@
+/*
+ Problem: Find the number that appears once
+  Approach1: Using Hashmap
+  traverse the array ,store the frequency of each number in hashmap
+  and traverse the hashmap and return the element whose frequency is 1.
+  TC: 0(n), SC: O(n)
+
+  Approach2 : Using XOR
+  a^a=0 and a^0=a;
+  TC: O(n),  SC: O(1);
+
+
+ */
+public class p012_Find_the_Number_Appearing_Once {
+    public static int getSingleElement(int arr[]){
+        int xor=0;
+        for(int num:arr){
+            xor^=num;
+        }
+        return xor;
+    }
+    public static void main(String[] args) {
+        int num[]={4,4,8,8,1,1,4};
+        System.out.println("The single element is: "+getSingleElement(num));
+    }
+}
