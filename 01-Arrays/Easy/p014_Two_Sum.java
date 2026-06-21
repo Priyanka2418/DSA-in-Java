8558f6c9058e90662bf4f89018ffb429308ebcb3
Priyanka Gupta <priyanka142024@gmail.com>
2026-06-21 11:39:56 +0530

    Two Sum Solution

---
+++ 01-Arrays/Easy/p014_Two_Sum.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class p014_Two_Sum {
+    public static String twoSumIndices(int arr[],int target){
+        int sum =0;
+        Map<Integer,Integer>map=new HashMap<>();
+        for(int i =0;i<arr.length;i++){
+            int complement= target-arr[i];
+            if(map.containsKey(complement)){
+                return "YES";
+                //return new int[]{map.get(complement),i};
+            }
+            map.put(arr[i],i);
+        }
+        return "NO";
+        //return new int[]{-1,-1}
+
+    }
+    public static void main(String[] args) {
+        int[] arr = {2, 6, 5, 8, 11};
+        int target = 14;
+
+        System.out.println(twoSumIndices(arr,target));
+    }
+}
