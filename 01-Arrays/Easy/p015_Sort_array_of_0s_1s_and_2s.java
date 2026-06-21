a4bd3b31f2ea0dfcdf2e9b585bd8b36336ed623a
Priyanka Gupta <priyanka142024@gmail.com>
2026-06-21 11:41:29 +0530

    Sort Zero One Two

---
+++ 01-Arrays/Easy/p015_Sort_array_of_0s_1s_and_2s.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class p015_Sort_array_of_0s_1s_and_2s {
+    public static void sortZeroOneTwo(int nums[]){
+        int count0=0;
+        int count1=0;
+        int count2=0;
+        for(int num:nums){
+            if(num == 0) count0++;
+            else if (num == 1) count1++;
+            else count2++;
+        }
+        for(int i =0;i<count0;i++){
+            nums[i]=0;
+        }
+        for(int i =count0;i<count0+count1;i++){
+            nums[i]=1;
+        }
+        for(int i =count0+count1;i<nums.length;i++){
+            nums[i]=2;
+        }
+
+    }
+    public static void main(String[] args) {
+        int[] nums = {0, 2, 1, 2, 0, 1};
+        System.out.println("After Sorting: ");
+        sortZeroOneTwo(nums);
+                for(int num:nums){
+                    System.out.print(num+" ");
+                }
+    }
+}
