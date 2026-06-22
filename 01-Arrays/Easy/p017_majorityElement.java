c805e078d11641fc46dd5c4b257e04ab31930c37
Priyanka Gupta <priyanka142024@gmail.com>
2026-06-22 09:15:58 +0530

    MajorityElement

---
+++ 01-Arrays/Easy/p017_majorityElement.java
@@ -0,0 +1,39 @@
+//PROBLEM: Find the Majority Element that occurs more than N/2 times
+/*
+Brute force: using nested loop
+TC: O(n)^2
+SC: O(1)
+
+Better Approach: using hashmap
+
+ */
+
+public class p017_majorityElement {
+    public static int majorityElement(int arr[]) {
+        int n = arr.length;
+        int count = 0;
+        int element = 0;
+        for (int i = 0; i < n; i++) {
+            if (count == 0) {
+                count = 1;
+                element = arr[i];
+            } else if (element == arr[i]) {
+                count++;
+            } else count--;
+        }
+        int count1 = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == element) {
+                count1++;
+            }
+        }
+        if (count1 > n / 2) {
+            return element;
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {2, 2, 1, 1, 1, 2, 2};
+        System.out.println("the majority element is: " + majorityElement(arr) );
+    }
+}
