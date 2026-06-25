0f9cd64bf024ca71822db37f7c7cb94e9a84c3d5
Priyanka Gupta <priyanka142024@gmail.com>
2026-06-25 22:04:20 +0530

    DSA updates

---
+++ src/arrays/medium/p024_setZeroes.java
@@ -0,0 +1,57 @@
+
+package src.arrays.medium;
+public class p024_setZeroes {
+    public static  void setZeroes(int nums[][]){
+        int m =nums.length;
+        int n =nums[0].length;
+
+        boolean firstRowZero=false, firstCoumnZero=false;
+        for(int i =0;i<n;i++){
+            if(nums[0][i]==0){
+                firstRowZero=true;
+                break;
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            if (nums[i][0] == 0) {
+                firstCoumnZero = true;
+                break;
+            }
+        }
+        for(int i =1;i<m;i++){
+            for(int j =1;j<n;j++){
+                if(nums[i][j]==0){
+                    nums[i][0]=0;
+                    nums[0][j]=0;
+                }
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (nums[i][0] == 0 || nums[0][j] == 0) {
+                    nums[i][j] = 0;
+                }
+            }
+        }
+        if (firstRowZero) {
+            for (int j = 0; j < n; j++) {
+                nums[0][j] = 0;
+            }
+        }
+        if(firstCoumnZero){
+            for(int i =0;i<m;i++){
+                nums[i][0]=0;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[][] matrix = {{0,1,2,0},{3,4,5,2},{1,3,1,5}};
+        setZeroes(matrix);
+        for(int[]row:matrix){
+            for(int val:row){
+                System.out.print(val+ " ");
+            }
+            System.out.println();
+        }
+    }
+}
