b81f2a3b37fab399bdd0dc2493ea82a25749e1b1
Priyanka Gupta <priyanka142024@gmail.com>
2026-06-27 22:47:12 +0530

    Implement Boyer-Moore voting algorithm

---
+++ src/arrays/hard/p029_majorityElement_II.java
@@ -0,0 +1,53 @@
+package src.arrays.hard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class p029_majorityElement_II {
+    public static List<Integer> majorityElement(int nums[]){
+        int n =nums.length;
+        int count1=0, count2=0;
+        int elem1=Integer.MIN_VALUE , elem2=Integer.MIN_VALUE;
+        for(int i =0;i<n;i++){
+            if(count1==0 && elem2!=nums[i]){
+                count1++;
+                elem1= nums[i];
+            } else if (count2==0 && elem1!=nums[i]) {
+                count2++;
+                elem2=nums[i];
+            } else if (nums[i]==elem1) {
+                count1++;
+            } else if (nums[i] ==elem2) {
+                count2++;
+            }else {
+                count1--;
+                count2--;
+            }
+        }
+        count1=0;
+        count2=0;
+        for(int i =0;i<n;i++){
+            if(nums[i]==elem1)count1++;
+            if (nums[i]==elem2)count2++;
+        }
+
+        int mini = n / 3 + 1;
+        List<Integer> result = new ArrayList<>();
+        if (count1 >= mini) result.add(elem1);
+        if (count2 >= mini && elem1 != elem2) result.add(elem2);
+
+        return result;
+    }
+    public static void main(String[] args) {
+        int[] arr = {11, 33, 33, 11, 33, 11};
+
+        p029_majorityElement_II sol= new p029_majorityElement_II();
+
+        List<Integer> ans = sol.majorityElement(arr);
+        System.out.print("the majority elements are: ");
+        for(int it:ans){
+            System.out.print(it +" ");
+        }
+        System.out.println();
+    }
+}
