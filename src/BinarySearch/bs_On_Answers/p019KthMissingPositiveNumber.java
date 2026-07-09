1087d4e73bf28971e55f117e6424bb374d29d45a
Priyanka Gupta <priyanka142024@gmail.com>
2026-07-09 23:35:11 +0530

    MissingPositiveNumber

---
+++ src/BinarySearch/bs_On_Answers/p019KthMissingPositiveNumber.java
@@ -0,0 +1,21 @@
+package src.BinarySearch.bs_On_Answers;
+
+public class p019KthMissingPositiveNumber {
+    public static int missingFinder(int arr[], int k ){
+        int low =0;
+        int high = arr.length-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            int missing=arr[mid]-(mid+1);
+            if(missing<k){
+                low=mid+1;
+            }else {
+                high=mid-1;
+            }
+        }
+        return k +high + 1;
+    }
+    public static void main(String[] args) {
+
+    }
+}
