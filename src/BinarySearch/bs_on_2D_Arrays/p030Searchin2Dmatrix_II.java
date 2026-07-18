14fcec5cba7487a2df2edf76321548d90b315e24
Priyanka Gupta <priyanka142024@gmail.com>
2026-07-18 22:29:28 +0530

    Implement row-wise binary search for Search a 2D Matrix II.

---
+++ src/BinarySearch/bs_on_2D_Arrays/p030Searchin2Dmatrix_II.java
@@ -0,0 +1,47 @@
+/*
+ * Problem: Search a 2D Matrix II (LeetCode 240)
+ *
+ * Approach: Start from the top-right corner. Move left if the current element
+ * is greater than the target, otherwise move down until the target is found
+ * or the search space is exhausted.
+ *
+ * Time Complexity: O(row + col)
+ * Space Complexity: O(1)
+ */
+
+package src.BinarySearch.bs_on_2D_Arrays;
+
+public class p030Searchin2Dmatrix_II {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row=matrix.length;
+        int col =matrix[0].length;
+
+        for(int i =0;i<row;i++){
+            if(matrix[i][0]<=target && target<=matrix[i][col-1]){
+                if(binarySearch(matrix[i], target))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean binarySearch(int matrix[], int target){
+        int n =matrix.length;
+        int low =0;
+        int high = n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(matrix[mid]==target)
+                return true;
+            else if(matrix[mid]<target){
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+
+    }
+}
