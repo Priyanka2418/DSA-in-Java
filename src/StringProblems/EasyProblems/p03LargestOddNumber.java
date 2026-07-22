09661ad73491fb48cd88e06308d47775305b9f33
Priyanka Gupta <priyanka142024@gmail.com>
2026-07-22 22:46:34 +0530

    Implement Largest Odd Number in a String using reverse traversal.

--- src/StringProblems/EasyProblems/p03LargestOddNumber.java
+++ src/StringProblems/EasyProblems/p03LargestOddNumber.java
@@ -1,33 +1,40 @@
 /*
 Problem: Largest Odd Number in a String
 
-Optimal:
-- Traverse from right to left.
-- Find the first odd digit.
-- Return substring(0, i + 1).
-- Time: O(n), Space: O(1)
+Approach:
+- Traverse from right to left to find the last odd digit.
+- Skip leading zeros in the resulting substring.
+- Return the largest odd-valued substring.
+
+Time Complexity: O(n)
+Space Complexity: O(1)
 */
-package src.stringProblems.EasyProblems;
+package src.StringProblems.EasyProblems;
 
 public class p03LargestOddNumber {
-    public String largestOddNumber(String s){
-        int index=-1;
-        //here we will if the last index is even we will break
-        for(int i =s.length()-1;i>=-;i--){
-            if(s.charAt(i)%2==1){
-                index=i;
+    public String largestOddNumber(String s) {
+
+        int index = -1;
+
+        // Find the rightmost odd digit
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if ((s.charAt(i) - '0') % 2 == 1) {
+                index = i;
                 break;
             }
-            //if no digit is found odd return an empty string
-            if(index==-1)return "";
-
-            // Skip leading zeroes up to the odd digit
-            i=0;
-            while(i<=index && s.charAt(i)=='0')i++;
-
-            //return substring from non-zero to odd digit
-            return s.substring(i,index+1);
         }
+
+        // No odd digit found
+        if (index == -1)
+            return "";
+
+        // Skip leading zeros
+        int start = 0;
+        while (start <= index && s.charAt(start) == '0') {
+            start++;
+        }
+
+        return s.substring(start, index + 1);
     }
     public static void main(String[] args) {
 
