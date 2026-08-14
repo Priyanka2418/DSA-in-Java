da478e3502771d3322bfe536576e33abe7926a1f
Priyanka Gupta <priyanka142024@gmail.com>
2026-08-14 10:54:06 +0530

    add one to linked list using recursive carry propagation

---
+++ src/LinkedList/MediumProblems_DLL/p025addOneRecursive.java
@@ -0,0 +1,32 @@
+/*
+Problem: Add 1 to a number represented by LL
+(solved this problem using recursive method)
+TC: O(n)
+SC: O(n) - recursion stack
+ */
+package src.LinkedList.MediumProblems_DLL;
+
+public class p025addOneRecursive {
+    public Node addOne(Node head){
+        int carry = addONeUtil(head);
+
+        if(carry!=0){
+            Node newHead= new Node (carry);
+            newHead.next=head;
+            head= newHead;
+        }
+        return head;
+    }
+    public int addONeUtil(Node node){
+        if(node==null)return 1;
+
+        int carry = addONeUtil(node.next);
+        int sum = node.data + carry;
+        node.data =sum%10;
+        return sum/10;
+    }
+    public static void main(String[] args) {
+
+    }
+
+}
